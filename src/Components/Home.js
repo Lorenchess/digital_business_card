3ce91db5dbc922e4a40e01687a03efe61aa146e0
Ramon Lorente <ramon_lorente@yahoo.com>
2022-02-04 05:12:53 -0500

    working on home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,26 @@
-import {Container, Row, Col} from 'react-bootstrap';
-import roberto from './assets/nobg-ledo-2.png';
+import {Container, Row, Col, Card, CardImg} from 'react-bootstrap';
+import { SiLichess } from "react-icons/si";
+import {FaPhoneAlt} from 'react-icons/fa';
+import {HiOutlineMail} from 'react-icons/hi';
+import roberto from './assets/nobg-ledo-3.png';
 import './Home.scss'
 
 const Home = () => {
    return (
-      <Container>
+      <Container fluid className='container-home'>
          <Row>
             <Col lg="6">
-               
-            </Col>
-            <Col lg="6">
               <img src={roberto} alt="roberto gomez" className='author'/>
+            </Col>
+            <Col lg="6" className='header-info'>
+               <h1>Classes Magistrales <SiLichess size={36}/></h1> 
+               
+               <h4>Para todos los niveles</h4>
+               <h4>Conozca los secretos de la escuela Cubana de Ajedrez</h4>
+               <h1 className='name'><strong>IM</strong> Roberto Carlos Gomez Ledo</h1>
+               <p className='results'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit tenetur molestias iste ullam minima cupiditate repellat placeat obcaecati quo. Laboriosam in temporibus cum eaque similique? Natus obcaecati consectetur laboriosam explicabo illum. Cumque, voluptatem consequuntur quaerat, laborum explicabo sequi atque odio, fuga illo culpa odit! Dolor nostrum repudiandae voluptas animi cupiditate?</p>
+               <p className='phone'> <FaPhoneAlt size={26} /> +34 600 836 909</p>
+               <p className='email'> <HiOutlineMail size={30} /> rcgledo@gmail.com</p>
             </Col>
          </Row>
       
